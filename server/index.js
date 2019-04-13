521d2e6a645605d320b0feb24c3251aebb1bb6c3
dscisn <dscisn@essex.ac.uk>
2019-04-14 01:44:54 +0200

    changed port assignment in order to work with heroku

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const compression = require('compression');
 
-const port = 3000;
+const port = process.env.PORT || '3000';
 const app = express();
 
 app.set('view engine', 'pug');
